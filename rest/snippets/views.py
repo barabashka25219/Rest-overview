6b1f279dd18dfb283fac852367af62215ea35983
v.svirskiy <barabashka25219@icloud.com>
2024-06-25 00:00:23 +0300

    Views refactoring: 1) Replace returned specified data type to dynamic data type by Request and Response objects. 2) Add format suffix to urls

--- rest/snippets/views.py
+++ rest/snippets/views.py
@@ -1,45 +1,61 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT, HTTP_404_NOT_FOUND
 
-@csrf_exempt
-def snippet_list(request):
+
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
     
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(data=data)
+        serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     
 
-@csrf_exempt
-def snippet_detail(request, pk):
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
     try:
         snippet = Snippet.objects.get(id=pk)
     except Snippet.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=HTTP_404_NOT_FOUND)
     
     if request.method == 'GET':
         serializer = SnippetSerializer(snippet)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SnippetSerializer(snippet, data=data)
+        serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return HttpResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
     
     elif request.method == 'DELETE':
         snippet.delete()
-        return HttpResponse(status=204)
+        return Response(status=HTTP_204_NO_CONTENT)
+    
+# Objects "Request", "Response" may handle more than one data format automatically.
+# Requests already are not bound to specified data format.
+#
+# Examples:
+# 
+# http http://127.0.0.1:8000/snippets/
+#
+# http http://127.0.0.1:8000/snippets/ Accept:application/json - get data in JSON format
+#
+# http http://127.0.0.1:8000/snippets/ Accept:text/html - get data in html format
+#
+# http http://127.0.0.1:8000/snippets.json  # JSON suffix
+#
+# http http://127.0.0.1:8000/snippets.api   # Browsable API suffix
+#
+# http --json POST http://127.0.0.1:8000/snippets/ code="print(456)" # send POST request with JSON payload
